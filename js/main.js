ed80173b04a8ed9849c630c91850c218d7ebc52f
Humberto Morales <humberto.morales.14@hotmail.com>
2023-01-12 08:47:09 -0800

    SS

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function drawBackground(background, context, sprites) {
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-loadImage('/img/tiles.png').then((image) => {
+loadImage('./img/tiles.png').then((image) => {
 	const sprites = new SpriteSheet(image, 16, 16);
 	sprites.define('ground', 0, 0);
 	sprites.define('sky', 3, 23);
