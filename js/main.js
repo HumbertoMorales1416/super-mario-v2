c6092667e833b262706cfd84eb2003be30d520e1
Humberto Morales <humberto.morales.14@hotmail.com>
2023-01-12 08:39:20 -0800

    Backgrounds & Tiles

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import SpriteSheet from './SpriteSheet.js';
+import { loadImage, loadLevel } from './loaders.js';
+
+function drawBackground(background, context, sprites) {
+	background.ranges.forEach(([x1, x2, y1, y2]) => {
+		for (let i = x1; i < x2; i++) {
+			for (let j = y1; j < y2; j++) {
+				sprites.drawTile(background.tile, context, i, j);
+			}
+		}
+	});
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+loadImage('/img/tiles.png').then((image) => {
+	const sprites = new SpriteSheet(image, 16, 16);
+	sprites.define('ground', 0, 0);
+	sprites.define('sky', 3, 23);
+
+	loadLevel('1-1').then((level) => {
+		level.backgrounds.forEach((background) => {
+			drawBackground(background, context, sprites);
+		});
+	});
+});
