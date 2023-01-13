82544e171f4b106b34934dd4b7a907e23a019cf3
Humberto Morales <humberto.morales.14@hotmail.com>
2023-01-13 11:38:27 -0800

    Sprites & Layers

--- js/main.js
+++ js/main.js
@@ -1,27 +1,46 @@
-import SpriteSheet from './SpriteSheet.js';
-import { loadImage, loadLevel } from './loaders.js';
-
-function drawBackground(background, context, sprites) {
-	background.ranges.forEach(([x1, x2, y1, y2]) => {
-		for (let i = x1; i < x2; i++) {
-			for (let j = y1; j < y2; j++) {
-				sprites.drawTile(background.tile, context, i, j);
-			}
-		}
-	});
-}
+import Compositor from './Compositor.js';
+import { createBackgroundLayer } from './layers.js';
+import { loadLevel } from './loaders.js';
+import { loadBackgroundSprites, loadMarioSprite } from './sprites.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-loadImage('./img/tiles.png').then((image) => {
-	const sprites = new SpriteSheet(image, 16, 16);
-	sprites.define('ground', 0, 0);
-	sprites.define('sky', 3, 23);
+function createSpriteLayer(sprite, pos) {
+	return function drawSpriteLayer(context) {
+		sprite.draw('idle', context, pos.x, pos.y);
+	};
+}
 
-	loadLevel('1-1').then((level) => {
-		level.backgrounds.forEach((background) => {
-			drawBackground(background, context, sprites);
-		});
-	});
+Promise.all([
+	loadMarioSprite(),
+	loadBackgroundSprites(),
+	loadLevel('1-1'),
+]).then(([marioSprite, backgroundSprites, level]) => {
+	const comp = new Compositor();
+
+	const backgroundLayer = createBackgroundLayer(
+		level.backgrounds,
+		backgroundSprites
+	);
+	comp.layers.push(backgroundLayer);
+
+	const pos = {
+		x: 64,
+		y: 64,
+	};
+
+	const spriteLayer = createSpriteLayer(marioSprite, pos);
+	comp.layers.push(spriteLayer);
+
+	console.log(comp);
+
+	function update() {
+		comp.draw(context);
+		pos.x += 2;
+		pos.y += 2;
+		requestAnimationFrame(update);
+	}
+
+	update();
 });
