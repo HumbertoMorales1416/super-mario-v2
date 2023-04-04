e74618d2080315d13747cd23312cddc2ecb4ac7f
Humberto Morales <humberto.morales.14@hotmail.com>
2023-04-04 10:29:31 -0700

    Tile Collision

--- js/entities.js
+++ js/entities.js
@@ -1,15 +1,17 @@
 import Entity from './Entity.js';
 import { loadMarioSprites } from './sprites.js';
+import Go from './traits/Go.js';
 import Jump from './traits/Jump.js';
 import Velocity from './traits/Velocity.js';
 
 export function createMario() {
 	return loadMarioSprites().then((sprite) => {
 		const mario = new Entity();
-		mario.addTrait(new Velocity());
-		mario.addTrait(new Jump());
+		mario.size.set(16, 16);
 
-		//console.log(mario.traits);
+		mario.addTrait(new Go());
+		mario.addTrait(new Jump());
+		//mario.addTrait(new Velocity());
 
 		mario.draw = function drawMario(context) {
 			sprite.draw('idle', context, this.pos.x, this.pos.y);
--- js/main.js
+++ js/main.js
@@ -1,49 +1,25 @@
-import Compositor from './Compositor.js';
 import Timer from './Timer.js';
 import { createMario } from './entities.js';
-import { createBackgroundLayer, createSpriteLayer } from './layers.js';
+import { setupKeyboard } from './inputs.js';
+import { createCollisionLayer } from './layers.js';
 import { loadLevel } from './loaders.js';
-import { loadBackgroundSprites, loadMarioSprites } from './sprites.js';
-
-import KeyboardState from './KeyboardState.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-Promise.all([createMario(), loadBackgroundSprites(), loadLevel('1-1')]).then(
-	([mario, backgroundSprites, level]) => {
-		const comp = new Compositor();
-		const backgroundLayer = createBackgroundLayer(
-			level.backgrounds,
-			backgroundSprites
-		);
-		comp.layers.push(backgroundLayer);
+Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
+	mario.pos.set(64, 180);
 
-		const gravity = 2000;
-		mario.pos.set(64, 180);
+	level.entities.add(mario);
 
-		const SPACE = 32;
-		const input = new KeyboardState();
-		input.addMapping(SPACE, (keyState) => {
-			if (keyState) {
-				mario.jump.start();
-			} else {
-				mario.jump.cancel();
-			}
-			console.log(keyState);
-		});
-		input.listenTo(window);
+	const input = setupKeyboard(mario);
+	input.listenTo(window);
 
-		const spriteLayer = createSpriteLayer(mario);
-		comp.layers.push(spriteLayer);
+	const timer = new Timer();
+	timer.update = function update(deltaTime) {
+		level.update(deltaTime);
+		level.comp.draw(context);
+	};
 
-		const timer = new Timer();
-		timer.update = function update(deltaTime) {
-			mario.update(deltaTime);
-			comp.draw(context);
-			mario.vel.y += gravity * deltaTime;
-		};
-
-		timer.start();
-	}
-);
+	timer.start();
+});
